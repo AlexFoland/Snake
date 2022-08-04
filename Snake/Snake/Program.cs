4aa4570cf4f148ea3e724b0a3ff92b20f127e4e1
Alex_Foland <folomeev.aleksei97@gmail.com>
2022-08-05 01:26:03 +0600

    Lesson 2

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLO ALEX");
+            Point p1 = new Point(14, 14, '%');  
+            p1.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(12);
+            numList.Add(234);
+            numList.Add(7);
+            List<Point> pointList = new List<Point>();
+            pointList.Add(p1);
+            pointList.Add(p1);
+            pointList.Add(p1);
+            foreach(Point alex in pointList)
+            {
+                Console.WriteLine(alex);
+            }
             Console.ReadLine();
         }
     }
