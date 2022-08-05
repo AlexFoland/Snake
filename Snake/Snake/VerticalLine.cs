9f55bd95999f34ffc9efb1eaa425c6b0654127c0
Alex_Foland <folomeev.aleksei97@gmail.com>
2022-08-06 02:08:21 +0600

    Lesson 3

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> plist;
+         public VerticalLine(int yTop, int yBottom, int x, char sym)
+        {
+            plist = new List<Point>();
+            for (int y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                plist.Add(p);
+            }
+        }
+        public void DrawVerticalLine()
+        {
+            foreach (Point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
