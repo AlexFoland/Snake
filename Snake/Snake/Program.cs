b43c039372dd39cb73732549163331991d1f4cfa
Alex_Foland <folomeev.aleksei97@gmail.com>
2022-08-30 18:42:44 +0600

    Snake 0.0.8

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -15,15 +16,32 @@ namespace Snake
             HorizontalLine downLine = new HorizontalLine(0,78,24,'+');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
             VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.DrawHorizontalLine();
-            downLine.DrawHorizontalLine();
-            leftLine.DrawVerticalLine();
-            rightLine.DrawVerticalLine();
+            upLine.DrawFigure();
+            downLine.DrawFigure();
+            leftLine.DrawFigure();
+            rightLine.DrawFigure();
 
-            Point p = new Point(4, 5, '*');
-            p.Draw();
+            Point p = new Point(7,9,'*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.DrawFigure();
 
-            Console.ReadLine();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.LeftArrow)
+                        snake.direction = Direction.LEFT;
+                    else if (key.Key == ConsoleKey.RightArrow)
+                        snake.direction = Direction.RIGHT;
+                    else if (key.Key == ConsoleKey.UpArrow)
+                        snake.direction = Direction.UP;
+                    else if (key.Key == ConsoleKey.DownArrow)
+                        snake.direction = Direction.DOWN;
+                }
+                snake.Move();
+                Thread.Sleep(100);
+            }
         }
     }
 }
